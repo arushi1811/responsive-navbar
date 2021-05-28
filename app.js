c087fdc54225826c1a6bdd149ce3ed070af6b81c
arushi1811 <arushig@bu.edu>
2021-05-28 16:52:54 -0400

    turn the hamburger into cross on click

--- app.js
+++ app.js
@@ -11,9 +11,14 @@ function setWithLi() {
 
 function burger() {
     const navlinks = document.querySelector('#navlinks');
+    const line1 = document.querySelector('#line1')
+    const line2 = document.querySelector('#line2')
+    const line3 = document.querySelector('#line3')
     const burger =document.querySelector('.burger').addEventListener('click',() => {
-        console.log("hi");
         navlinks.classList.toggle("links-active");
+        line1.classList.toggle("line1-active");
+        line2.classList.toggle("line2-active");
+        line3.classList.toggle("line3-active");
     });
     
 }
